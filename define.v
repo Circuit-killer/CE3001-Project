ecda15ab5b85d6382a166b3023e413773383cb89
Lu Shengliang <lushl9301@gmail.com>
2013-11-03 14:04:16 +0800

    modified define/ change bad indent of *_mem.v

--- define.v
+++ define.v
@@ -4,6 +4,9 @@
  `define ISIZE 16
  `define DSIZE 16
  `define RSIZE 4
+ `define MEM_SPACE 16
+ `define MAX_LINE_LENGTH 6 //not sure
+ `define MEM_SPACE 16 //not sure
 
  `define ADD 0
  `define SUB 1
