fb1e9a74286a7893a9ee5411457a9ade7d5c141c
Richard Lu <lmjrichard@gmail.com>
2013-11-03 21:57:05 +0800

    random

--- CPU.v
+++ CPU.v
@@ -7,26 +7,29 @@ module CPU(Clk, Rst);
   
   bufNum 	signalName 	signalWidth
   =====Fetch=====
+  
+  =====Decode=====
   0 	     Instruction	16
   1 	     CurrPC		    16
-  =====Decode=====
+  
   2 	     ALUOp		     3
   3 	     Signal      (0:10)
 	        MemEnab     (11)
 	        MemWrite    (12)
 	        WriteEn		   (13)
 	                    14
-  4	      RData1		    16
-  5	      RData2		    16
+  
   6	      Sign_Ext8	  16
   7	      Sign_Ext12	 16
   =====Execute=====
+  4	      RData1		    16
+  5	      RData2		    16
   8       MuxOut[5]   16
-  9	      FLAG		      3
-  10	      Out		       16
   =====Mem Access=====
-  11	     MemData_out	16
+  9	      FLAG		      3
+  10	     ALUOut		    16
   =====Write Back=====
+  11	     MemData_out	16
 ******************************************/
   
   integer i;
@@ -40,32 +43,32 @@ module CPU(Clk, Rst);
   
   wire [15:0] MuxOut [0:10];
   wire [15:0] AddOut [0:1];
-  wire [15:0] OrOut;
+  wire [15:0] LHBOut;
     
   //Multiplexer Implementation
   assign MuxOut[0] = ID_Buff[3][0]?MuxOut[1]:AddOut[0];
   assign MuxOut[1] = ID_Buff[3][1]?ID_Buff[5]:MuxOut[2];
-  assign MuxOut[2] = ID_Buff[3][2]?AddOut[2]:ID_Buff[7]; 
+  assign MuxOut[2] = ID_Buff[3][2]?AddOut[1]:ID_Buff[7]; 
   assign MuxOut[3] = MEM_Buff[3][3]?16'd15:MEM_Buff[0][11:8];
   assign MuxOut[4] = ID_Buff[3][4]?ID_Buff[4]:ID_Buff[6];
   assign MuxOut[5] = ID_Buff[3][5]?ID_Buff[5]:ID_Buff[6];
-  assign MuxOut[6] = EX_Buff[3][6]?AddOut[0]:MuxOut[10];  
+  assign MuxOut[6] = EX_Buff[3][6]?EX_Buff[1]:MuxOut[10];  
   assign MuxOut[7] = MEM_Buff[3][7]?MEM_Buff[11]:MuxOut[6];
   assign MuxOut[8] = ID_Buff[3][8]?ID_Buff[0][11:8]:ID_Buff[0][3:0];
   assign MuxOut[9] = ID_Buff[3][9]?Spec_Addr_Reg:MuxOut[0];
-  assign MuxOut[10] = EX_Buff[3][10]?OrOut:EX_Buff[10];
+  assign MuxOut[10] = EX_Buff[3][10]?LHBOut:EX_Buff[10];
   
   //Implement addition logic
-  assign AddOut[0] = IF_Buff[1] + 16'b1;
-  assign AddOut[1] = AddOut[0] + ID_Buff[6];
+  //assign AddOut[0] = IF_Buff[1] + 16'b1;
+  assign AddOut[1] = ID_Buff[1] + ID_Buff[6];
   
   //Implement or logic
-  assign OrOut = ID_Buff[5][7:0] | (ID_Buff[6]<<8);
+  assign LHBOut = {ID_Buff[5][7:0],ID_Buff[6][7:0]);
   
   //Module Instantiation
   /**********Instruction Fectch**********/
   I_memory A0(.address(MuxOut[9]), .data_out(IF_Buff[0]), .Clk(Clk), .Rst(Rst));
-  PC A1(.Clk(Clk), .Rst(Rst), .NextPC(IF_Buff[1]), .CurrPC(MuxOut[9]));
+  PC A1(.Clk(Clk), .Rst(Rst), .CurrPC(MuxOut[9]), .NextPC(IF_Buff[1]));
   
   /**********Instruction Decode**********/
   control A2(.OpCode(IF_Buff[0][15:12]), .Cond(IF_Buff[0][3:0]), .Flag(EX_Buff[9]), .ALUOp(ID_Buff[2]), .WriteEn(ID_Buff[3][13]), .MemEnab(ID_Buff[3][11]), .MemWrite(ID_Buff[3][12]), .Signal(ID_Buff[3][10:0]));
--- PC.v
+++ PC.v
@@ -1,17 +1,17 @@
-module PC(Clk, Rst, NextPC, CurrPC);
+module PC(Clk, Rst, CurrPC, NextPC);
   
   //declare input and output signals
-  input [15:0] NextPC;
+  input [15:0] CurrPC;
   input Clk, Rst;
   
-  output reg [15:0] CurrPC;
+  output reg [15:0] NextPC;
   
   always @(posedge Clk)
   begin
     if (!Rst)
-      CurrPC <= 0;
+      NextPC <= 0;
     else
-      CurrPC <= NextPC;   
+      NextPC <= CurrPC + 1;
   end
     
 endmodule
