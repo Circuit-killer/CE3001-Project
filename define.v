114505e3dcfacabffd592fbb69b0110d4d4c8259
Lu Shengliang <lushl9301@gmail.com>
2013-11-14 01:49:37 +0800

    update define.v  & Set MEM_SPACE to 16

--- define.v
+++ define.v
@@ -4,8 +4,8 @@
  `define ISIZE 16
  `define DSIZE 16
  `define RSIZE 4
- `define MEM_SPACE 16 //not sure
- `define MAX_LINE_LENGTH 80 //not sure
+ `define MEM_SPACE 16
+ `define MAX_LINE_LENGTH 80
 
  `define ADD 0
  `define SUB 1
@@ -15,5 +15,13 @@
  `define SRL 5
  `define SRA 6
  `define RL 7
+ `define LW 8
+ `define SW 9
+ `define LHB 10
+ `define LLB 11
+ `define B 12
+ `define JAL 13
+ `define JR 14
+ `define EXEC 15
 
 `endif
